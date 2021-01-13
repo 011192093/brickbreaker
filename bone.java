fee1cc18c070cff9bd60c1ae64c880db52d09eec
Farhee Aalina Ahmad <52394353+011192093@users.noreply.github.com>
2021-01-13 09:17:15 +0600

    Update bone.java

--- bone.java
+++ bone.java
@@ -10,9 +10,10 @@ public class bone extends JPanel implements KeyListener, ActionListener {
     int initial_tray_pos=260;
     int initial_ball_xpos=100;
     int initial_ball_ypos=300;
-    int ball_xspeed=1;
+    int ball_xspeed=3;
     int ball_yspeed=3;
 
+     obstacle ob;
     Timer timer;
     public bone(){
         addKeyListener(this);
@@ -23,8 +24,13 @@ public class bone extends JPanel implements KeyListener, ActionListener {
         i=new ImageIcon("E:\\background.png").getImage();
         i1=new ImageIcon("E:\\background.png").getImage();
 
-        timer=new Timer(2,this);
+        timer=new Timer(20,this);
         timer.start();
+
+        ob=new obstacle(3,6);
+    }
+    public void draw (Graphics2D g){
+        ob.paint(g);
     }
     public void paint (Graphics g){
         //background
@@ -38,7 +44,7 @@ public class bone extends JPanel implements KeyListener, ActionListener {
         a.fillRect(593,0, 7,500);
         a.fillRect(0, 493,600,7);
         //tray
-        GradientPaint color=new GradientPaint(0,0,Color.YELLOW,420,0,Color.RED);
+        GradientPaint color=new GradientPaint(0,0,Color.BLACK,420,0,Color.darkGray);
         a.setPaint(color);
         a.fillRect(initial_tray_pos, 450, 50,10);
         a.setStroke(new BasicStroke(3));
@@ -48,6 +54,8 @@ public class bone extends JPanel implements KeyListener, ActionListener {
         GradientPaint color1=new GradientPaint(0,0,Color.BLUE,420,0,Color.ORANGE);
         a.setPaint(color1);
         a.fillOval(initial_ball_xpos,initial_ball_ypos,20,20);
+        //obstacles
+        ob.paint((Graphics2D) g);
         a.dispose();
     }
 
@@ -63,7 +71,7 @@ public class bone extends JPanel implements KeyListener, ActionListener {
         char press= ke.getKeyChar();
         if(press=='a'||press=='A'){
             if(initial_tray_pos>7){
-                initial_tray_pos=initial_tray_pos-30;
+                initial_tray_pos=initial_tray_pos-15;
                 repaint();
 
             }
@@ -75,7 +83,7 @@ public class bone extends JPanel implements KeyListener, ActionListener {
         }
         else if(press=='d'||press=='D'){
             if(initial_tray_pos<543){
-                initial_tray_pos=initial_tray_pos+30;
+                initial_tray_pos=initial_tray_pos+15;
                 repaint();
             }
             else if(initial_tray_pos>=543){
@@ -91,26 +99,52 @@ public class bone extends JPanel implements KeyListener, ActionListener {
     @Override
     public void actionPerformed(ActionEvent ae) {
         timer.start();
-        if(new Rectangle(initial_ball_xpos,initial_ball_ypos,20,20).intersects(new Rectangle(initial_tray_pos, 450, 50,10)))
-        {
-            ball_yspeed=ball_yspeed*(-1);
-            ball_xspeed=ball_xspeed*(-1);
+        //bouncing from tray
+        if(new Rectangle(initial_ball_xpos,initial_ball_ypos,20,20).intersects(new Rectangle(initial_tray_pos, 450, 50,10))) {
+            ball_yspeed = ball_yspeed * (-1);
+            ball_xspeed = ball_xspeed * (-1);
 
         }
-       if(initial_ball_xpos>=573){
+
+        //bouncing off borders
+
+        if(initial_ball_xpos>=573){
             ball_xspeed=ball_xspeed*(-1);
         }
         if(initial_ball_xpos<=7){
             ball_xspeed=ball_xspeed*(-1);
         }
         initial_ball_xpos= initial_ball_xpos+ball_xspeed;
-        if(initial_ball_ypos>=473){
-            ball_yspeed=ball_yspeed*(-1);
-        }
+
         if(initial_ball_ypos<=7){
             ball_yspeed=ball_yspeed*(-1);
         }
-        initial_ball_ypos= initial_ball_ypos+ball_yspeed;
+        if(initial_ball_ypos>=473){
+            ball_yspeed=ball_yspeed*(-1);
+            // System.out.println("failed");
+        }
+
+        //finding obstacle
+        for(int k=0;k<ob.bricks.length;k++){
+            for(int l=0;l<ob.bricks[0].length;l++){
+                if(ob.bricks[k][l]>0){
+                    int bh=ob.height;
+                    int bw=ob.width;
+                    int bxpos= l *ob.width +ob.x;
+                    int bypos= k * ob.height +ob.y;
+
+                    if(new Rectangle(initial_ball_xpos,initial_ball_ypos,20,20).intersects(new Rectangle(bxpos, bypos, bw,bh))){
+                        ball_xspeed=ball_xspeed*(-1);
+                        ball_yspeed=ball_yspeed*(-1);
+                        ob.invisible(k,l);
+                    }
+
+                }
+            }
+        }
+
+       initial_ball_ypos= initial_ball_ypos+ball_yspeed;
+        initial_ball_xpos= initial_ball_xpos+ball_xspeed;
         repaint();
     }
 
