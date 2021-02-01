690f2d8b0523967fd7c4e281a0eacbe8fa077780
Farhee Aalina Ahmad <52394353+011192093@users.noreply.github.com>
2021-02-01 14:11:49 +0600

    Update startpage.java

--- startpage.java
+++ startpage.java
@@ -2,8 +2,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.*;
+import java.net.Socket;
 
-public class startpage {//extends JFrame {
+public class startpage {
 
     public static void main(String[] args) {
 
@@ -20,26 +22,35 @@ public class startpage {//extends JFrame {
         panel.setBackground(Color.BLACK);
         frame.setContentPane(panel);
 
-        JButton button1=new JButton("start");
+        JLabel label1 =new JLabel("Welcome to Brick-Breaker");
+        label1.setFont(new Font("Serif", Font.BOLD, 40));
+        label1.setBounds(50,50,500,50);
+
+        JButton button1=new JButton("START PLAYING");
         button1.setBackground(Color.orange);
-        button1.setFont(new Font("Arial", Font.BOLD, 40));
-        button1.setBounds(215,50,150,50);
+        button1.setFont(new Font("Serif", Font.BOLD, 30));
+        button1.setBounds(100,150,380,50);
 
-        JLabel label=new JLabel("Enter your name here");
+        JLabel label=new JLabel("Enter Your Name Here");
         label.setFont(new Font("Arial", Font.BOLD, 20));
-        label.setBounds(100,120,250,50);
+        label.setBounds(105,220,250,50);
 
-        JTextField field =new JTextField("...");
+        JTextField field =new JTextField("unknown");
         field.setFont(new Font("Arial", Font.BOLD, 20));
-        field.setBounds(310,130,150,30);
+        field.setBounds(320,230,150,30);
+        field.setHorizontalAlignment(SwingConstants.CENTER);
+        field.setBackground(Color.BLACK);
+
 
 
        panel.repaint();
+       panel.add(label1);
        panel.add(button1);
        panel.add(label);
        panel.add(field);
-        System.out.println(field.getText());
 
+
+       System.out.println(field.getText());
 
     button1.addActionListener(new ActionListener() {
         @Override
@@ -48,9 +59,14 @@ public class startpage {//extends JFrame {
 
             brick s = new brick(field.getText());
 
+
         }
     });
 
 
+
+
     }
+
+
 }
